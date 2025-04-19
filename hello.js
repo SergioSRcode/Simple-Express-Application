fb9e5066ef41022e87b0c11d7f8837b037b879da
Sergio Ravera <sergioravera@hotmail.de>
2025-04-19 19:39:51 +0200

    included the morgan module for logging on requests

--- hello.js
+++ hello.js
@@ -1,29 +1,41 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
+app.use(morgan("common"));
+
+// const writeLog = (req, res) => {
+//   let timeStamp = String(new Date()).slice(4, 24); // Mmm dd YYYY HH:MM:SS
+//   console.log(`${timeStamp} ${req.method} ${req.originalUrl} ${res.statusCode}`);
+// };
 
 app.get("/", (req, res) => {
   res.redirect("/english");
+  // writeLog(req, res);
 });
 
 app.get("/english", (req, res) => {
   res.render("hello-world-english");
+  // writeLog(req, res);
 });
 
 app.get("/french", (req, res) => {
   res.render("hello-world-french");
+  // writeLog(req, res);
 });
 
 app.get("/serbian", (req, res) => {
   res.render("hello-world-serbian");
+  // writeLog(req, res);
 });
 
 app.get("/german", (req, res) => {
   res.render("hello-world-german");
+  // writeLog(req, res);
 });
 
 app.listen(3000, "localhost", () => {
