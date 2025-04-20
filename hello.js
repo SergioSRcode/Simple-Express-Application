308ad21003f7050dffd63f2a13ecf702dfa1892f
Sergio Ravera <sergioravera@hotmail.de>
2025-04-20 16:25:56 +0200

    added an Error Handler alongside URL-parameter input validation

--- hello.js
+++ hello.js
@@ -29,6 +29,13 @@ const COUNTRY_DATA = [
   },
 ];
 
+const LANGUAGE_CODES = {
+  english: "en-US",
+  french: "fr-FR",
+  serbian: "sr-Cryl-rs",
+  german: "de-DE",
+};
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
@@ -43,36 +50,25 @@ app.get("/", (req, res) => {
   res.redirect("/english");
 });
 
-app.get("/english", (req, res) => {
-  res.render("hello-world-english", {
-    countries: COUNTRY_DATA,
-    currentPath: req.path,
-    language: "en-US",
-  });
+app.get("/:language", (req, res, next) => {
+  const language = req.params.language;
+  const languageCode = LANGUAGE_CODES[language];
+  
+  if (!languageCode) {
+    next(new Error(`Language not supported: "${language}"`));
+  } else {
+    res.render(`hello-world-${language}`, {
+      countries: COUNTRY_DATA,
+      currentPath: req.path,
+      language: LANGUAGE_CODES[language],
+    });
+  }
 });
 
-app.get("/french", (req, res) => {
-  res.render("hello-world-french", {
-    countries: COUNTRY_DATA,
-    currentPath: req.path,
-    language: "fr-FR",
-  });
-});
-
-app.get("/serbian", (req, res) => {
-  res.render("hello-world-serbian", {
-    countries: COUNTRY_DATA,
-    currentPath: req.path,
-    language: "sr-Dyrl-rs",
-  });
-});
-
-app.get("/german", (req, res) => {
-  res.render("hello-world-german", {
-    countries: COUNTRY_DATA,
-    currentPath: req.path,
-    language: "de-DE",
-  });
+// error handler
+app.use((err, req, res, _next) => {
+  console.log(err);
+  res.status(404).send(err.message);
 });
 
 app.listen(3000, "localhost", () => {
