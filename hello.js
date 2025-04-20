ea7e641f8e43841fa26b3c723c5105cdf4791bb4
Sergio Ravera <sergioravera@hotmail.de>
2025-04-20 15:21:54 +0200

    added an each loop to avoid repetition upon creating a list of languages

--- hello.js
+++ hello.js
@@ -2,6 +2,33 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+const COUNTRY_DATA = [
+  {
+    path: "/english",
+    flag: "flag-of-United-States-of-America.png",
+    alt: "US Flag",
+    title: "Go to US English site",
+  },
+  {
+    path: "/french",
+    flag: "flag-of-France.png",
+    alt: "Drapeau de la france",
+    title: "Aller sur le site français",
+  },
+  {
+    path: "/serbian",
+    flag: "flag-of-Serbia.png",
+    alt: "Застава Србије",
+    title: "Идите на српски сајт",
+  },
+  {
+    path: "/german",
+    flag: "flag-of-Germany.png",
+    alt: "Deutschland Flagge",
+    title: "Zur deutschen Seite",
+  },
+];
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
@@ -18,6 +45,7 @@ app.get("/", (req, res) => {
 
 app.get("/english", (req, res) => {
   res.render("hello-world-english", {
+    countries: COUNTRY_DATA,
     currentPath: req.path,
     language: "en-US",
   });
@@ -25,6 +53,7 @@ app.get("/english", (req, res) => {
 
 app.get("/french", (req, res) => {
   res.render("hello-world-french", {
+    countries: COUNTRY_DATA,
     currentPath: req.path,
     language: "fr-FR",
   });
@@ -32,6 +61,7 @@ app.get("/french", (req, res) => {
 
 app.get("/serbian", (req, res) => {
   res.render("hello-world-serbian", {
+    countries: COUNTRY_DATA,
     currentPath: req.path,
     language: "sr-Dyrl-rs",
   });
@@ -39,6 +69,7 @@ app.get("/serbian", (req, res) => {
 
 app.get("/german", (req, res) => {
   res.render("hello-world-german", {
+    countries: COUNTRY_DATA,
     currentPath: req.path,
     language: "de-DE",
   });
